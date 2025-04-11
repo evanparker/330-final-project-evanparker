f10f26a9d6a3a2eb88b7d983f7e6685384425522
Evan Parker <parkevan@gmail.com>
2025-04-11 15:10:14 -0700

    support ip v6 for reverse proxy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongoURL =
   process.env.DB_URL || "mongodb://127.0.0.1/every-mini-painted-cloudinary";
 
 mongoose.connect(mongoURL, {}).then(() => {
-  server.listen(port, "0.0.0.0", () => {
+  server.listen(port, "::", () => {
     console.log(`Server is listening on http://localhost:${port}`);
   });
 });
